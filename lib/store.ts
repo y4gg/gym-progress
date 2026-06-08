062d5eb5c438b3ad027e6191ff0b01476fc6383d
y4gg <contact@y4.gg>
2026-06-08 19:05:10 +0200

    Add Zustand

---
+++ lib/store.ts
@@ -0,0 +1,67 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { Workout, Exercise, Store } from "./types";
+
+const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      workouts: [],
+      addWorkout: (workout: Workout) =>
+        set((state) => ({ workouts: [...state.workouts, workout] })),
+      addExercise: (exercise: Exercise, workoutId: string) =>
+        set((state) => ({
+          workouts: state.workouts.map((workout) => {
+            if (workout.id === workoutId) {
+              return {
+                ...workout,
+                exercises: [...workout.exercises, exercise],
+              };
+            }
+
+            return workout;
+          }),
+        })),
+      editWorkout: (updatedWorkout: Workout) =>
+        set((state) => ({
+          workouts: state.workouts.map((workout) => {
+            if (workout.id === updatedWorkout.id) {
+              return updatedWorkout;
+            }
+
+            return workout;
+          }),
+        })),
+      editExercise: (updatedExercise: Exercise) =>
+        set((state) => ({
+          workouts: state.workouts.map((workout) => ({
+            ...workout,
+            exercises: workout.exercises.map((exercise) => {
+              if (exercise.id === updatedExercise.id) {
+                return updatedExercise;
+              }
+
+              return exercise;
+            }),
+          })),
+        })),
+      deleteWorkout: (workoutId: string) =>
+        set((state) => ({
+          workouts: state.workouts.filter(
+            (workout) => workout.id !== workoutId,
+          ),
+        })),
+      deleteExercise: (exerciseId: string) =>
+        set((state) => ({
+          workouts: state.workouts.map((workout) => ({
+            ...workout,
+            exercises: workout.exercises.filter(
+              (exercise) => exercise.id !== exerciseId,
+            ),
+          })),
+        })),
+    }),
+    {
+      name: "store",
+    },
+  ),
+);
---
+++ lib/types.ts
@@ -0,0 +1,28 @@
+interface Workout {
+  id: string;
+  name: string;
+  exercises: Exercise[];
+}
+
+interface Exercise {
+  id: string;
+  name: string;
+  workoutId: string;
+  weight: number;
+  sets: number;
+  maxReps?: number; // If set not null: Increase weight suggestion is enabled
+  logging: boolean;
+  notes: string;
+}
+
+interface Store {
+  workouts: Workout[];
+  addWorkout: (workout: Workout) => void;
+  addExercise: (exercise: Exercise, workoutId: string) => void;
+  editWorkout: (workout: Workout) => void;
+  editExercise: (exercise: Exercise) => void;
+  deleteWorkout: (workoutId: string) => void;
+  deleteExercise: (exerciseId: string) => void;
+}
+
+export type { Workout, Exercise, Store };
