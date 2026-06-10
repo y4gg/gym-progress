24ea89d02a0690844caa5b565f5839a608e25997
y4gg <contact@y4.gg>
2026-06-10 14:23:26 +0200

    Implement exercise creation form with state management and input handling

--- app/w/[id]/create/page.tsx
+++ app/w/[id]/create/page.tsx
@@ -4,10 +4,14 @@ import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Plus, Minus, Info } from "lucide-react";
-import { use } from "react";
 import { useStore } from "@/lib/store";
 import { useState } from "react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Exercise } from "@/lib/types";
+import { createId } from "@paralleldrive/cuid2";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { use } from "react";
 
 export default function CreateExercisePage({
   params,
@@ -16,18 +20,48 @@ export default function CreateExercisePage({
 }) {
   const { id } = use(params);
   const workout = useStore((state) => state.getWorkoutById(id));
-
   if (!workout) {
     return <div>Workout not found</div>;
   }
 
+  const [exercise, setExercise] = useState<Exercise>({
+    id: createId(),
+    name: "",
+    weight: 0,
+    notes: "",
+    sets: 0,
+    logging: false,
+    workoutId: id,
+  });
+
+  const handleCreateExercise = () => {
+    // TODO: validate input
+    useStore.getState().addExercise(exercise, workout.id);
+    useRouter().push(`/w/${workout.id}`);
+    toast.success("Exercise created successfully!");
+  };
+
   return (
     <div className="grid sm:p-24 mx-auto max-w-7xl gap-2 p-6">
       <h1 className="text-4xl font-bold">Create Exercise</h1>
+      <div className="grid gap-1">
+        <Label>Exercise Name</Label>
+        <Input
+          value={exercise.name}
+          onChange={(e) => setExercise({ ...exercise, name: e.target.value })}
+        />
+      </div>
       <div>
         <Label>Weight</Label>
         <div className="relative mt-1">
-          <Input type="number" className="pr-8" />
+          <Input
+            type="number"
+            className="pr-8"
+            value={exercise.weight}
+            onChange={(e) =>
+              setExercise({ ...exercise, weight: Number(e.target.value) })
+            }
+          />
           <span className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 text-sm text-muted-foreground">
             kg
           </span>
@@ -35,7 +69,11 @@ export default function CreateExercisePage({
       </div>
       <div>
         <Label>Notes</Label>
-        <Textarea className="mt-1" />
+        <Textarea
+          className="mt-1"
+          value={exercise.notes}
+          onChange={(e) => setExercise({ ...exercise, notes: e.target.value })}
+        />
       </div>
       <div>
         <Label>Sets</Label>
@@ -44,7 +82,14 @@ export default function CreateExercisePage({
             <Plus />
           </Button>
           <div className="relative">
-            <Input type="number" className="pr-8" />
+            <Input
+              type="number"
+              className="pr-8"
+              value={exercise.sets}
+              onChange={(e) =>
+                setExercise({ ...exercise, sets: Number(e.target.value) })
+              }
+            />
             <span className="pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 text-sm text-muted-foreground">
               sets
             </span>
@@ -62,14 +107,21 @@ export default function CreateExercisePage({
           <Info />
         </Button>
       </div>
-      <div>
-        <Label className="mb-1">Logging</Label>
-        <ToggleGroup type="single" variant={"outline"} defaultValue="false">
+      <div className="grid gap-1">
+        <Label>Logging</Label>
+        <ToggleGroup
+          type="single"
+          variant={"outline"}
+          defaultValue="false"
+          onValueChange={(value) =>
+            setExercise({ ...exercise, logging: value === "true" })
+          }
+        >
           <ToggleGroupItem value="true">Enabled</ToggleGroupItem>
           <ToggleGroupItem value="false">Disabled</ToggleGroupItem>
         </ToggleGroup>
       </div>
-      <Button>Create Exercise</Button>
+      <Button onClick={handleCreateExercise}>Create Exercise</Button>
     </div>
   );
 }
