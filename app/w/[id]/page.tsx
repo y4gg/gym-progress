ad3928b3e9595b8d5c155541ca50be637cf148d2
y4gg <contact@y4.gg>
2026-06-10 13:27:32 +0200

    Workout Overview

---
+++ app/w/[id]/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { use } from "react";
+import { useStore } from "@/lib/store";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Exercise } from "@/lib/types";
+import { Edit } from "lucide-react";
+
+export default function WorkoutOverview({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = use(params);
+  const workout = useStore((state) => state.getWorkoutById(id));
+
+  if (!workout) {
+    return <div>Workout not found</div>;
+  }
+
+  const exercises = workout.exercises;
+
+  return (
+    <div className="flex flex-col items-center max-w-xl mx-auto py-10 gap-1">
+      <div className="w-full">
+        <Link href={`/w/${id}/create`}>
+          <Button>Create Exercise</Button>
+        </Link>
+      </div>
+      {exercises.map((exercise: Exercise) => (
+        <div className="flex w-full gap-0.5" key={exercise.id}>
+          <Button
+            asChild
+            variant={"default"}
+            className="flex-1 justify-between text-left"
+          >
+            <Link href={`/w/${exercise.id}?redirect=true`}>
+              <span>{exercise.name}</span>
+            </Link>
+          </Button>
+          <Button variant="secondary" size="icon">
+            <Edit />
+          </Button>
+        </div>
+      ))}
+    </div>
+  );
+}
