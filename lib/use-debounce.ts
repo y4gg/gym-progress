075aae19b28f82008cb21ce87d62fa85fea2bec9
y4gg <contact@y4.gg>
2026-06-10 23:20:11 +0200

    debounce hook

---
+++ lib/use-debounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(cb: number, delay: number) {
+  const [debounceValue, setDebounceValue] = useState(cb);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(cb);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [cb, delay]);
+  return debounceValue;
+}
